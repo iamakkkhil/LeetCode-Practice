fe4932e7b180453f84278727e2133f809dd00eda
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-10-06 20:56:39 +0530

    unique email and shortest length to a character added

---
+++ Strings/812_Shortest_distance_to_a_character/solution.py
@@ -0,0 +1,25 @@
+def shortest_distance(s, c):
+    ans = []
+    inf = float('inf')
+    prev = inf
+
+    # for-loop from start -> end
+    for i in range(len(s)):
+        if s[i] == c:
+            prev = i
+        if prev == inf:
+            ans.append(inf)
+        else:
+            ans.append(abs(prev-i))
+
+    # for-loop from end -> start
+    prev = inf
+    for i in range(len(s)-1, -1, -1):
+        if s[i] == c:
+            prev = i
+        ans[i] = min(abs(prev-i), ans[i])
+
+    return ans
+
+
+print(shortest_distance('loveleetcode', 'e'))
---
+++ Strings/929_Unique_email_address/solution.py
@@ -0,0 +1,21 @@
+def unique_email(emails):
+    corrected_emails = []
+    for email in emails:
+        domain_name = ""
+        local_name = ""
+        plus = False
+        for i in range(len(email)):
+            if email[i] == "+":
+                plus = True
+            elif plus == False and email[i] != "." and email[i] != "@":
+                local_name += email[i]
+            elif email[i] == "@":
+                domain_name = email[i:]
+                corrected_emails.append(local_name+domain_name)
+                break
+    
+    print(len(set(corrected_emails)))
+
+
+emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+unique_email(emails)
