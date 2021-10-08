6efe987d9ba2a17cdb025a89db32c6ccd23519c1
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-10-09 00:16:22 +0530

    Problem 599 added

---
+++ Strings/599_Minimum_index_sum_of_two_lists/solution.py
@@ -0,0 +1,17 @@
+def findRestaurant(list1, list2):
+        common = []
+        sum_ = float('inf')
+        rank  = {restuarant : i for i,restuarant in enumerate(list1)}
+        
+        for j, res in enumerate(list2):
+            if res in rank:
+                if rank[res]+j < sum_:
+                    common = [res]
+                    sum_ = rank[res]+j
+                elif rank[res]+j == sum_:
+                    common.append(res)
+        return common
+
+list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+list2 = ["KFC","Burger King","Tapioca Express","Shogun"]
+print(findRestaurant(list1, list2))
