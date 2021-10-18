a1adead1f1e8073c3842018357e52dc184f4e735
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-10-18 17:58:36 +0530

    alternate sol for 997

--- Graphs/997_Find_the_Town_Judge/solution.py
+++ Graphs/997_Find_the_Town_Judge/solution.py
@@ -1,16 +1,30 @@
-def findJudge(self, n: int, trust: List[List[int]]) -> int:
-    if n==1:
-        return 1
+def findJudge(n, trust):
+    # if n==1:
+    #     return 1
     
-    people = []
-    judge = []
-    for group in trust:
-        people.append(group[0])
-        judge.append(group[1])
+    # people = []
+    # judge = []
+    # for group in trust:
+    #     people.append(group[0])
+    #     judge.append(group[1])
         
-    judge_Set = set(judge)
-    for i in judge_Set:
-        if (i not in people) and (judge.count(i)==n-1):
-            return i
+    # judge_Set = set(judge)
+    # for i in judge_Set:
+    #     if (i not in people) and (judge.count(i)==n-1):
+    #         return i
+    # return -1
+
+    arr = [0]*n
+    for a,b in trust:
+        arr[a-1] -= 1
+        arr[b-1] += 1
+
+    if  max(arr) == n - 1: 
+        return arr.index(n - 1) + 1
     return -1
+    
+    
         
+n = 3
+trust = [[1,3],[2,3],[3,1]]
+print(findJudge(n, trust))
