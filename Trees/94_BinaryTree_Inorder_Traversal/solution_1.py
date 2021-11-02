1f2ab205db740dad6218212032f6d4f1eaafe58f
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-11-02 19:03:27 +0530

    Question 94 added

---
+++ Trees/94_BinaryTree_Inorder_Traversal/solution_1.py
@@ -0,0 +1,25 @@
+"""
+Recursive Approach
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def utilInorderTraversal(self, root: Optional[TreeNode], res):
+        if root==None:
+            return
+        if root.left != None:
+            self.utilInorderTraversal(root.left, res)
+        res.append(root.val)    
+        if root.right != None:
+            self.utilInorderTraversal(root.right, res)
+    
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        self.utilInorderTraversal(root, res)
+        return res
---
+++ Trees/94_BinaryTree_Inorder_Traversal/solution_2.py
@@ -0,0 +1,26 @@
+"""
+Iterative Approach
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        current = root
+        
+        while(current or stack):
+            if current:
+                stack.append(current)
+                current = current.left
+            else:
+                current = stack.pop()
+                res.append(current.val)
+                current = current.right
+        
+        return res
