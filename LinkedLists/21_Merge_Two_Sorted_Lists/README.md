c902c96bff9169da95415fedc1092d8af2c3f39c
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-11-03 19:05:04 +0530

    Question 21 Added

---
+++ LinkedLists/21_Merge_Two_Sorted_Lists/README.md
@@ -0,0 +1,44 @@
+# [21. Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/)
+
+**Difficulty** : Easy
+
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+---
+
+## Example 1:
+![img](https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg)
+
+```
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+```
+
+## Example 2:
+
+```
+Input: l1 = [], l2 = []
+Output: []
+```
+
+## Example 3:
+
+```
+Input: l1 = [], l2 = [0]
+Output: [0]
+```
+
+## Constraints:
+
+* The number of nodes in both lists is in the range ```[0, 50]```.
+* ```-100 <= Node.val <= 100```
+* Both ```l1``` and ```l2``` are sorted in non-decreasing order.
+
+<br>
+
+# Submission Details
+
+* 208 / 208 test cases passed.
+* Status: Accepted
+* Runtime: 40 ms
+* Memory Usage: 14.3 MB
---
+++ LinkedLists/21_Merge_Two_Sorted_Lists/solution.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    #Solution 1
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        final = ListNode()
+        current = final
+        
+        while(l1!=None and l2!=None):
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+                
+            current = current.next
+            
+        if(l1!=None):
+            current.next = l1
+        
+        if(l2!=None):
+            current.next = l2
+            
+        return final.next
+                
+    #Solution 2
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        while(l1!=None or l2!=None):
+            v1, v2 = 101, 101
+            
+            if (l1!=None):
+                v1=l1.val
+            if (l2!=None):
+                v2=l2.val
+                
+            if v1>=v2:
+                val = v2
+                l2 = l2.next
+            else:
+                val = v1
+                l1 = l1.next
+            
+            current = ListNode(val, None)
+            
+            if result == None:
+                result = current
+                previous = result
+            else:
+                previous.next = current
+                previous = current
+            
+        return result
