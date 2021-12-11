35274aa1c93f408d4ac1e1df9a4a4626017b2c22
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-12-11 11:31:51 +0530

    Question1234

--- Strings/1234-Replace_the_Substring_for_Balanced_String/solution.py
+++ Strings/1234-Replace_the_Substring_for_Balanced_String/solution.py
@@ -1,27 +1,35 @@
-class Solution:
-    
-    def check_satisfy(self, s, p1, p2, balanced_count):
-        count_chars = {"Q":0, "W":0, "E":0, "R":0}
-        new_s = s[:p1]+s[p2:]
-        for i in new_s:
-            count_chars[i] += 1
-            if count_chars[i] > balanced_count:
-                return False
-        return True
+def check_satisfy(balanced_count, count_chars):
+    for i in count_chars:
+        if count_chars[i] > balanced_count:
+            return False
+    return True
 
 
-    def balancedString(self, s: str) -> int:
-        balanced_count = len(s)/4 
-        substring_count = len(s)
+def balancedString(s: str) -> int:
+    balanced_count = len(s) / 4
+    substring_count = len(s)
+    p1, p2 = 0, 0
+    count_chars = {"Q": 0, "W": 0, "E": 0, "R": 0}
+    for i in s:
+        count_chars[i] += 1
 
-        p1,p2 = 0,0
+    while p2 >= p1 and p2 < len(s)+1:
+        if check_satisfy(balanced_count, count_chars):
+            count_chars[s[p1]] += 1
+            p1 += 1
+            if substring_count > (p2 - p1 + 1):
+                substring_count = p2 - p1 + 1
 
-        while(p2>=p1 and p2<=len(s)):
-            if self.check_satisfy(s, p1, p2, balanced_count):
-                p1 += 1
-                if substring_count > (p2-p1+1):
-                    substring_count = p2-p1+1
-            else:
-                p2+=1
-        
-        return substring_count
+        else:
+            if p2 >= len(s):
+                return substring_count
+            var = s[p2]
+            count_chars[var] -= 1
+            p2 += 1
+
+    return substring_count
+
+
+if __name__ == "__main__":
+    s = "WQWRQQQW"
+    print(balancedString(s))
