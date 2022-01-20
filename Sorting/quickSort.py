0dee2cade39f006cdf5127c5eae90e31d347dbbe
Akhil Bhalerao <iamakhil1408@gmail.com>
2022-01-20 22:50:51 +0530

    pratice questions

---
+++ Sorting/quickSort.py
@@ -0,0 +1,21 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[j], arr[i] = arr[i], arr[j]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quicksort(arr, low, high):
+    if low < high:
+        pivot = partition(arr, low, high)
+        quicksort(arr, low, pivot - 1)
+        quicksort(arr, pivot + 1, high)
+
+
+arr = [8, 0, 1, 6, 9, 5]
+quicksort(arr, 0, len(arr) - 1)
+print(arr)
