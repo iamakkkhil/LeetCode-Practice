d0eb168abc5dada74a54de1ad8f2234c23821ea3
Akhil Bhalerao <iamakhil1408@gmail.com>
2022-01-25 10:57:18 +0530

    1981. Minimize the Difference Between Target

---
+++ Arrays/1981. Minimize the Difference Between Target and Chosen Elements/solution.py
@@ -0,0 +1,12 @@
+def minimizeTheDifference(mat, target) -> int:
+    R, C = len(mat), len(mat[0])
+
+    S = set(mat[0])
+    for r in range(1, R):
+        NS = set()
+        for x in set(mat[r]):
+            for y in S:
+                NS.add(x + y)
+        S = NS
+
+    return min(abs(x - target) for x in S)
